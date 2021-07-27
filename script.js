f1a0e38e610355907dcbc4659292801d5f59463b
Patrícia Borges <79218267+patborges@users.noreply.github.com>
2021-07-27 20:46:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function search(city) {
 
 function handleSubmit (event){
    event.preventDefault();
-   let cityInputElement = document.querySelector(#city-input);
+   let cityInputElement = document.querySelector("#city-input");
    search(cityInputElement.value);
 }
 
