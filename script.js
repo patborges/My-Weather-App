b42335c5700e8ac93667e2a28ab8fa5c8eb4bc59
Patrícia Borges <79218267+patborges@users.noreply.github.com>
2021-07-27 15:39:53 +0100

    Update units' convertion

--- script.js
+++ script.js
@@ -89,16 +89,35 @@ function displayTemperature(response) {
   getForecast(response.data.coord);
 }
 
-function convert(degree) {
-  var x;
-  if (degree == "C") {
-    x = document.getElementById("c").value * 9 / 5 + 32;
-    document.getElementById("f").value = Math.round(x);
-  } else {
-    x = (document.getElementById("f").value -32) * 5 / 9;
-    document.getElementById("c").value = Math.round(x);
-  }
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 
 function search(city) {
   let apiKey = "388afacfb92bd4f649accab582225b09";
