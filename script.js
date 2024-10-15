b239bafad9f5e4e6b6bc78294540764c32deff0c
patborges <p.alexandra.borges@gmail.com>
2024-10-15 14:12:43 +0100

    Added Sunday forecast to weather app and updated script to display 7 days

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7) {
       forecastHTML = forecastHTML + 
       `
       <div class="col-2">
